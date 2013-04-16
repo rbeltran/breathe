409f8c223ac01fe98bac03539a1c255133e5e854
Robert Beltran <robert@jphat.com>
2013-04-15 18:16:04 -0700

    buttons now disappear and reappear properly

--- src/com/jphat/Breathe/MainActivity.java
+++ src/com/jphat/Breathe/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends Activity {
 
 				for( Button bcButton: basicCourtesyButtons ) {
 			if( buttonsVisible ) {
-				bcButton.setVisibility( View.INVISIBLE );
+				bcButton.setVisibility( View.GONE );
 			} else {
 				bcButton.setVisibility( View.VISIBLE );
 			}
